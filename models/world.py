9aeb09992b4cf3071e9e49178dc3361bc93de856
titanmind <sharpnova@gmail.com>
2025-06-03 19:44:52 -0500

    Generate star system on world init

--- models/world.py
+++ models/world.py
@@ -1,6 +1,8 @@
 from typing import List
+
 from core.events import EventBus
 from models.star_system import StarSystem
+from services.procgen_service import ProcGenService
 
 
 class World:
@@ -10,6 +12,11 @@ class World:
         self.bus = bus
         self.systems: List[StarSystem] = []
         self.active_system: StarSystem | None = None
+
+        procgen = ProcGenService()
+        system = procgen.generate_star_system()
+        self.systems.append(system)
+        self.active_system = system
 
     def update(self, dt: float) -> None:
         if self.active_system:
